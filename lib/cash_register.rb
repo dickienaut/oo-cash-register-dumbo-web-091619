70bbfa29cb72d955adbc6ead4bb121e3701995b6
lyle dickie <lyle.e.dickie@gmail.com>
2019-09-19 02:52:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction 
+  
+  def initialize(discount=nil)
+    @total =  0
+    @discount = discount
+    @last_transaction = nil
+    @items = []
+  end
 
+  def add_item(item_name, item_price, quantity=1)
+    self.total = self.total += item_price * quantity
+    self.last_transaction = item_price * quantity
+    quantity.to_i.times do
+      self.items << item_name
+    end  
+  end
+
+
+
+  def apply_discount
+    if self.discount
+     self.total = self.total * ((100 - self.discount) / 100).to_i
+    else
+     'sorry'
+   end
+ end
+ 
+end
